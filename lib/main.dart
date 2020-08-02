7e8d8dd662af9e2d32b7fc4402a47c751be4d3bf
harshthakur066 <harshthakur066@gmail.com>
2020-08-02 13:20:45 +0530

    added products grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
+      home: ProductOvervireScreen(),
     );
   }
 }
