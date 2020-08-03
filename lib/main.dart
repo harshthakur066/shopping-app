72b018aaefd89beef55f68340df8a0f191917d93
harshthakur066 <harshthakur066@gmail.com>
2020-08-03 20:47:40 +0530

    added cart provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './providers/cart.dart';
 import './providers/products.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -10,9 +11,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      // create: (context) => Products(),
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          // create: (context) => Products(),
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          // create: (context) => Products(),
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
