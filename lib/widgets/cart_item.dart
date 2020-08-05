8c260e0f05595e02f7e613fb9b92528ef569b61f
harshthakur066 <harshthakur066@gmail.com>
2020-08-05 22:37:35 +0530

    added dialog before deleting item from cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -34,6 +34,29 @@ class CartItem extends StatelessWidget {
       ),
       direction: DismissDirection.endToStart,
       onDismissed: (direction) => cart.removeItem(productId),
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from cart ?'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: Text('No'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        );
+      },
       child: Card(
         margin: EdgeInsets.symmetric(
           vertical: 5,
