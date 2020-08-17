964f333c46b39f3da3608addeeea9bcaf6ca7075
harshthakur066 <harshthakur066@gmail.com>
2020-08-17 23:18:26 +0530

    restricted prod crud data to the user

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -9,9 +9,14 @@ import './edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchProducts(true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
+    // final productsData = Provider.of<Products>(context);
+    print('rebuilding...');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
@@ -26,29 +31,29 @@ class UserProductsScreen extends StatelessWidget {
       ),
       drawer: AppDrawer(),
       body: FutureBuilder(
-        future: productsData.fetchProducts(true),
+        future: _refreshProducts(context),
         builder: (ctx, snapshot) =>
             snapshot.connectionState == ConnectionState.waiting
                 ? Center(
                     child: CircularProgressIndicator(),
                   )
                 : RefreshIndicator(
-                    onRefresh: () async {
-                      await productsData.fetchProducts(true);
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.all(8),
-                      child: ListView.builder(
-                        itemCount: productsData.items.length,
-                        itemBuilder: (_, i) => Column(
-                          children: [
-                            UserProductItem(
-                              productsData.items[i].id,
-                              productsData.items[i].title,
-                              productsData.items[i].imageUrl,
-                            ),
-                            Divider(),
-                          ],
+                    onRefresh: () => _refreshProducts(context),
+                    child: Consumer<Products>(
+                      builder: (ctx, productsData, _) => Padding(
+                        padding: EdgeInsets.all(8),
+                        child: ListView.builder(
+                          itemCount: productsData.items.length,
+                          itemBuilder: (_, i) => Column(
+                            children: [
+                              UserProductItem(
+                                productsData.items[i].id,
+                                productsData.items[i].title,
+                                productsData.items[i].imageUrl,
+                              ),
+                              Divider(),
+                            ],
+                          ),
                         ),
                       ),
                     ),
